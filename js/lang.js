bfecd190561f323a21bdc0b5c248f7ec1b7be99a
Andrey Ionov <62856803+PerlAndrey@users.noreply.github.com>
2021-08-29 22:23:33 +0300

    Add files via upload

--- js/lang.js
+++ js/lang.js
@@ -1,26 +1,20 @@
-const langArr = {
-    "unit" :  {
-        "ru": "Добро пожаловать на викторину по JavaScript",
-        "en": "build a multi language site ",
-    }, 
-    "chip": {
-        "ru": "графический чип",
-        "en": "graphics processing unit",
-    }, 
-    "memory": {
-        "ru": "объем памяти",
-        "en": "memory size",
-    }, 
-    "memory-type": {
-        "ru": "тип памяти",
-        "en": "memory type",
-    }, 
-    "cool": {
-        "ru": "тип системы охлаждения",
-        "en": "сooling system type",
-    }, 
-    "more": {
-        "ru": "детально о товаре",
-        "en": "more details",
-    }, 
-}
+let rus = {
+    hello: 'Добро пожаловать на викторину по JavaScript',
+    lorem: '<p>У вас есть 75 секунд, чтобы ответить на вопросы, проверяющие ваши знания основ JavaScript.</p><p>Неправильные ответы имеют 10-секундный штраф!</p><p>Получите ли вы высокий балл?</p><p>УДАЧИ!</p>',
+    butbegin:'Начать',  
+};
+  
+  let eng = {
+    hello: 'WELCOME TO THE JAVASCRIPT CODE QUIZ!',
+    lorem: '<p>You have 75 seconds to answer questions testing your knowledge of JavaScript fundamentals.</p> <p>Incorrect answers have a 10 second penalty!</p><p>Will you get the high score?</p><p>GOOD LUCK!</p>',
+    butbegin:'Start',  
+};
+  
+  changeLagnuage();
+  
+  function changeLagnuage(){
+    let language = lang.checked ? rus : eng;
+    document.querySelectorAll('[text]').forEach(el => {
+      el.innerHTML = language[el.getAttribute('text')];
+    })
+  }
